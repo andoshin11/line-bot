4e4c55f57b5794901c41ccd9ed5af11ed7bc4d83
andoshin11 <shinglish11@gmail.com>
2018-01-23 11:54:19 +0900

    Add admin.js

---
+++ functions/admin.js
@@ -0,0 +1,8 @@
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+
+// initialize
+admin.initializeApp(functions.config().firebase)
+
+module.exports = admin
+
